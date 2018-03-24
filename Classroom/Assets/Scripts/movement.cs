4418b2411b369e12642cfc9289ad31a314c69e57
Hao Li <haoli@dhcp-hol-920.eduroam.cornell.edu>
2018-03-24 19:13:06 -0400

    added movement file

---
+++ Classroom/Assets/Scripts/movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	
+	private float moveSpeed;
+
+	void Start () {
+		moveSpeed = 0.50f;
+	}
+
+	void Update () {
+		transform.Translate (moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}
